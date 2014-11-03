f597dda40cd8511a80a71e9108c22dcd4f40b5e3
Frekkenbok <eva.minion@yandex.ru>
2014-11-03 20:53:27 +0300

    First commit

---
+++ Partitions/src/partitions/Partitions.java
@@ -0,0 +1,15 @@
+
+package partitions;
+
+/**
+ *
+ * @author Евгения
+ */
+public class Partitions {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
